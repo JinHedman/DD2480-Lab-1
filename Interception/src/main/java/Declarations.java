dc94babe5d9e85cd869aeb49287acdbc2d9fd786
Love-goransson <lovegoransson@outlook.com>
2025-01-28 15:36:48 +0100

    Close issue #22
    feat: the added function compute_lic1_0 now tests if it possible to contain the consecutive points
    in a circle with radius RADIUS1. If it not possible does it return true, false otherwise. The appropriate tests for
    true and false are also added.

--- Interception/src/main/java/Declarations.java
+++ Interception/src/main/java/Declarations.java
@@ -76,8 +76,65 @@ public class Declarations {
         return false;
     }
 
-    
+    // Need to return the smallest possible circle 
+    public boolean compute_lic_1(){
+        for(int i = 2; i< NUMPOINTS; i++){
+            double a = points[i-2].distance(points[i]);  
+            double b = points[i-1].distance(points[i]);           
+            double c = points[i-1].distance(points[i-2]);    
+            
+            // if any of the points are a distance greater then 2 *params.R can we return immediately.
+            if (a > 2*params.RADIUS1 || b >  2*params.RADIUS1 || c >  2*params.RADIUS1){
+                // We found a triple that does NOT fit => return true 
+                return true;
+            }
+            double area = heronsRule(a,b,c);
+            // Need the longest side.
+            double longest = Math.max(a, Math.max(b, c));
+            double neededRadius;
+            // If area is zero are they colinear and we compute half the distance of the longestt
+            if(area == 0){
+                neededRadius = longest / 2.0;
+            }else{
+                /*
+                 * Here do we have two cases. the neededRadius = (a * b * c) / (4.0 * area) formula is the 
+                 * formula that passes through the vertices which is correct for sharp anges, but with obstuse/right angles is the longest side the diameter. 
+                 * There is also a special case for when all are colinear. 
+                 */
+                double x, y;
+                if (longest == a) {
+                    x = b; y = c;
+                } else if (longest == b) {
+                    x = a; y = c;
+                } else {
+                    x = a; y = b;
+                }
+                // right/obtuse if longest^2 >= x^2 + y^2
+                if (longest*longest >= x*x + y*y) {
+                    // The minimal circle is diameter = "longest"
+                    neededRadius = longest / 2.0;
+                } else {
+                    // acute => use circumcircle radius: R = (abc)/(4 * area)
+                    neededRadius = (a * b * c) / (4.0 * area);
+                }
+            }
+            // return true if the radius is greater. 
+            if (neededRadius > params.RADIUS1) {
+                return true; 
+            }
+        }
+        return false; 
+    }
 
+
+    /*
+     * Help function for calculating the area of a triangle given its three sides.
+     */
+    public double heronsRule(double a, double b, double c){
+        double s = (a+b+c)/2;
+        double A = Math.sqrt((s*(s-a)*(s-b)*(s-c)));
+        return A; 
+    }
 
 
     // test code
