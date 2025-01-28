9beb2f95a95e6e728fdd64068c63e18aaa541837
Love-goransson <59106275+Love-goransson@users.noreply.github.com>
2025-01-28 10:55:32 +0100

    Update DecideTest.java
    Added declarations Interception.src.main.java.Declarations; as  an import.

--- Interception/src/test/java/DecideTest.java
+++ Interception/src/test/java/DecideTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import Interception.src.main.java.Decide;
 import Interception.src.main.java.Parameters;
 import Interception.src.main.java.Point;
+import Interception.src.main.java.Declarations;
 
 class DecideTest {
     
