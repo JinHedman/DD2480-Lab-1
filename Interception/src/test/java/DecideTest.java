cf20ff0eda6b2134df71048464cae383d5e90c5a
Love-goransson <lovegoransson@outlook.com>
2025-01-28 12:53:39 +0100

    closes issue #20 Have added a function for determining if two consequtive points in the list of points are distance greater than LENGTH1 apart from each other. Have also added tests for when we expect the function to return a false and when we expect it to return true.

--- Interception/src/test/java/DecideTest.java
+++ Interception/src/test/java/DecideTest.java
@@ -1,7 +1,6 @@
 
 package Interception.src.test.java; 
 
-// this is solved at compile time if nothing else. 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.annotation.Target;
@@ -15,5 +14,32 @@ import Interception.src.main.java.Declarations;
 
 class DecideTest {
     
+
+    /*
+     * Function for testing if LIC0 is true, which means testing if there are two points at a distance greater than length LENGTH1 from each other. 
+     * It also tests if LIC0 returns false when it is supposed to, which is when there are no consequtive points at a distance greater than 
+     * LENGTH1 from each other. 
+     */ 
+    @Test
+    void testLic0True(){
+        Parameters params = new Parameters();
+        params.LENGTH1 = 3; 
+        int NUMPOINTS = 3;
+        
+        Point[] points = new Point[] {new Point(1, 5), new Point(5, 4), new Point(5, 6) };     
+        
+        Declarations trueDecide = new Declarations(NUMPOINTS, points, params, null, null);
+       
+        assertTrue(trueDecide.compute_lic_0());
+
+        Point[] falsePoints = new Point[] {new Point(1, 2), new Point(2, 3), new Point(3, 4) };     
+        
+        Declarations falseDecide = new Declarations(NUMPOINTS, falsePoints, params, null, null);
+        assertFalse(falseDecide.compute_lic_0());
+
+
+    }
+
+   
     
 }
