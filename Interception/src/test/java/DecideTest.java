80c5d9b23ce407cd17851c392b7f483fd7e63a66
Love-goransson <lovegoransson@outlook.com>
2025-01-29 11:17:31 +0100

    Closes issue #34 feat: Added code which returns the correct boolean for the conditions in LIC3. Also added the appropriate tests.

--- Interception/src/test/java/DecideTest.java
+++ Interception/src/test/java/DecideTest.java
@@ -65,6 +65,58 @@ class DecideTest {
 
 
     }
-   
+
+
+    /*
+     * Function for testing if LIC3 returns the correct boolean values.
+     * Returns true when the three points
+     * Returns false when there are less than three points, the points can not form a triangle, the area is negative or the area is less than AREA1.
+     */
+    @Test
+    void testLIC3(){
+
+        // should return true when there is a triangle with area greater than AREA1.
+        Parameters params = new Parameters();
+        
+        params.AREA1 = 1.0;
+        int NUMPOINTS=  5;
+        Point [] truePoints = new Point[] { new Point(0, 0),new Point(2, 0), new Point(0, 2), new Point(1, 1), new Point(3, 3)  };
+        Declarations trueDecide = new Declarations(NUMPOINTS, truePoints, params, null, null);
+        assertTrue(trueDecide.compute_lic_3(), "Should return true when at least one triangle has area greater than AREA1.");
+    
+
+        // Test that it returns false if the number of points is less than three.
+        params.AREA1 = 1.0;
+        NUMPOINTS = 2;
+        Point[] toFewPoints = new Point[] { new Point(0, 0),new Point(1, 1)};
+        Declarations falseDecide = new Declarations(NUMPOINTS, toFewPoints, params, null, null);
+        assertFalse(falseDecide.compute_lic_3(), "Should return false when less than three points are provided.");
+
+        //Should return false when area is negative.
+        params.AREA1 = -1.0;
+        NUMPOINTS = 3;
+        Point[] negativePoints = new Point[] {new Point(0, 0),new Point(1, 0), new Point(0, 1)};
+        Declarations negativeArea = new Declarations(NUMPOINTS, negativePoints, params, null, null);
+        assertFalse(negativeArea.compute_lic_3(), "Should return false when less than three points are provided.");
+
+        // Should return false when the triangle is degenerate. 
+        params.AREA1 = 1;
+        NUMPOINTS = 4;
+        Point[] degenPoints = new Point[] {new Point(0, 0), new Point(1, 1), new Point(2, 2), new Point(3, 3)};
+        Declarations degenArea = new Declarations(NUMPOINTS, degenPoints, params, null, null);
+        assertFalse(degenArea.compute_lic_3(), "Should return false when AREA1 is zero but all triangles are degenerate.");
+    
+
+        // Shoule return false when there is no triangle with area greater than AREA1.
+        params.AREA1 = 1.0;
+        NUMPOINTS = 5; 
+        Point[] points = new Point[] {new Point(0, 0),new Point(1, 0), new Point(1, 1),new Point(2, 1), new Point(3, 1)};
+        Declarations smallArea = new Declarations(NUMPOINTS, points, params, null, null);
+        assertFalse(smallArea.compute_lic_3(), "Should return false when no triangle has area greater than AREA1.");
+        
+        
+
+    }
+
     
 }
